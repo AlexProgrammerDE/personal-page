bda9cd618f171ab61598781a243c5ac8589b00ef
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-03-11 10:21:45 +0100

    Use tailwind for font

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,12 +6,13 @@ import {Inter} from "next/font/google";
 const inter = Inter({
   display: "swap",
   subsets: ["latin"],
+  variable: '--font-inter',
 })
 
 const Layout = ({children}: { children: ReactNode }) => {
   return (
       <div
-          className={"min-h-screen min-w-screen flex flex-col z-50 bg-background text-text scroller " + inter.className}>
+          className={`min-h-screen min-w-screen flex flex-col z-50 bg-background text-text scroller ${inter.variable} font-sans`}>
         <NavBar/>
         {children}
         <Footer/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,48 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} \*/
 module.exports = {
-    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}",],
-    theme: {
-        extend: {
-            colors: {
-                background: "#222433",
-                brighter: "#1d1e2c",
-                section: "#c74d5f",
-                sectionDark: "#963747",
-                sectionDarkest: "#85333f",
-                content: "#2EB086",
-                text: "#EEE6CE"
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      },
+      colors: {
+        background: "#222433",
+        brighter: "#1d1e2c",
+        section: "#c74d5f",
+        sectionDark: "#963747",
+        sectionDarkest: "#85333f",
+        content: "#2EB086",
+        text: "#EEE6CE"
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              marginBottom: "0.5rem",
             },
-            typography: {
-                DEFAULT: {
-                    css: {
-                        h1: {
-                            marginBottom: "0.5rem",
-                        },
-                        h2: {
-                            marginTop: "1rem",
-                            marginBottom: "0.5rem",
-                        },
-                        h3: {
-                            marginTop: "1rem",
-                            marginBottom: "0.5rem",
-                        },
-                        p: {
-                            marginTop: "0.5rem",
-                            marginBottom: "0.5rem",
-                        },
-                        img: {
-                            marginTop: "0.75em",
-                            marginBottom: "0.75em",
-                        }
-                    },
-                },
+            h2: {
+              marginTop: "1rem",
+              marginBottom: "0.5rem",
             },
+            h3: {
+              marginTop: "1rem",
+              marginBottom: "0.5rem",
+            },
+            p: {
+              marginTop: "0.5rem",
+              marginBottom: "0.5rem",
+            },
+            img: {
+              marginTop: "0.75em",
+              marginBottom: "0.75em",
+            }
+          },
         },
+      },
     },
-    plugins: [require('@tailwindcss/typography')],
+  },
+  plugins: [require('@tailwindcss/typography')],
 }
